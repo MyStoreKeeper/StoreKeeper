6ef45e283b6332c8074b19dd6706e74d1c4bb109
wwb1990 <358140200@qq.com>
2016-03-15 03:41:11 +0800

    test2
    test

--- src/window/ModelStoreApplication.java
+++ src/window/ModelStoreApplication.java
@@ -5,6 +5,8 @@ public class ModelStoreApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("1111");
+		System.out.print("1111");
+		System.out.print("1111");
 	}
 
 }
