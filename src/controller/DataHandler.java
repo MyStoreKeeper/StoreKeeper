c183d69aa1477b06ccc67ea3e6d8e9dd62f8b13c
privin <vinayakpriya1108@gmail.com>
2016-03-15 14:59:02 +0800

    Tasks Assignment

--- src/controller/DataHandler.java
+++ src/controller/DataHandler.java
@@ -23,6 +23,46 @@ public class DataHandler {
 	
 	public static DataHandler data;
 	
+	public ArrayList<Member> getMemberList() {
+		return memberList;
+	}
+
+	public void setMemberList(ArrayList<Member> memberList) {
+		this.memberList = memberList;
+	}
+
+	public ArrayList<Discount> getDiscountList() {
+		return discountList;
+	}
+
+	public void setDiscountList(ArrayList<Discount> discountList) {
+		this.discountList = discountList;
+	}
+
+	public ArrayList<Category> getCategoryList() {
+		return categoryList;
+	}
+
+	public void setCategoryList(ArrayList<Category> categoryList) {
+		this.categoryList = categoryList;
+	}
+
+	public ArrayList<Storekeeper> getStorekeeperList() {
+		return storekeeperList;
+	}
+
+	public void setStorekeeperList(ArrayList<Storekeeper> storekeeperList) {
+		this.storekeeperList = storekeeperList;
+	}
+
+	public ArrayList<Transaction> getTransactionList() {
+		return transactionList;
+	}
+
+	public void setTransactionList(ArrayList<Transaction> transactionList) {
+		this.transactionList = transactionList;
+	}
+
 	public DataHandler() {
 		// TODO Auto-generated constructor stub
 	}
