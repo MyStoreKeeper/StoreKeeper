e362870b006d29d69230125ed683c121dd1608c2
Premraj-Murugaraj <premraj.murugaraj@yahoo.com.sg>
2016-03-16 12:37:56 +0800

    Object_Task v1.2

--- src/objects/Transaction_Item.java
+++ src/objects/Transaction_Item.java
@@ -8,20 +8,74 @@ package objects;
 
 public class Transaction_Item {
 	
-	private Transaction aTransaction;
+	
 	private int transactionID;
+	private String productID;
+	private String memberID;
+	private int quantityPurchased;
+	private String date;
 	
 	
-	public void setTransaction(Transaction aTransaction) {
-		this.aTransaction = aTransaction;
+	public Transaction_Item(int transactionID, String productID, String memberID, int quantityPurchased, String date) {
+		
+		this.transactionID = transactionID;
+		this.productID = productID;
+		this.memberID = memberID;
+		this.quantityPurchased = quantityPurchased;
+		this.date = date;
 	}
+
+
 	public int getTransactionID() {
 		return transactionID;
 	}
+
+
 	public void setTransactionID(int transactionID) {
 		this.transactionID = transactionID;
 	}
+
+
+	public String getProductID() {
+		return productID;
+	}
+
+
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+
+
+	public String getMemberID() {
+		return memberID;
+	}
+
+
+	public void setMemberID(String memberID) {
+		this.memberID = memberID;
+	}
+
+
+	public int getQuantityPurchased() {
+		return quantityPurchased;
+	}
+
+
+	public void setQuantityPurchased(int quantityPurchased) {
+		this.quantityPurchased = quantityPurchased;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
 	
+		
 	
 }
 
