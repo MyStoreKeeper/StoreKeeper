f576a46674bdb91646042f27efdaa772c2103407
Premraj-Murugaraj <premraj.murugaraj@yahoo.com.sg>
2016-03-16 12:33:22 +0800

    Object_Task v1.1
    review_comments(Puja)_-Amended

--- src/objects/Transaction.java
+++ src/objects/Transaction.java
@@ -1,4 +1,7 @@
 package objects;
+
+import java.util.ArrayList;
+
 /**
  * 
  * @author Premraj M
@@ -6,51 +9,18 @@ package objects;
  */
 
 public class Transaction  {
-	private int transactionID;
-	private String productID;
-	private String memberID;
-	private int quantityPurchased;
-	private String date;
-	public Transaction(int transactionID, String productID, String memberID, int quantityPurchased, String date) {
+	
+	private Transaction_Item aTransaction;
+	
+	ArrayList<Transaction_Item> generateTxn= new ArrayList<Transaction_Item>();
+	
+	
+	public Transaction_Item getaTransaction() {
+		return aTransaction;
+	}
+	public void setaTransaction(Transaction_Item aTransaction) {
+		this.aTransaction = aTransaction;
+	}
 		
-		this.transactionID = transactionID;
-		this.productID = productID;
-		this.memberID = memberID;
-		this.quantityPurchased = quantityPurchased;
-		this.date = date;
-	}
-	public int getTransactionID() {
-		return transactionID;
-	}
-	public void setTransactionID(int transactionID) {
-		this.transactionID = transactionID;
-	}
-	public String getProductID() {
-		return productID;
-	}
-	public void setProductID(String productID) {
-		this.productID = productID;
-	}
-	public String getMemberID() {
-		return memberID;
-	}
-	public void setMemberID(String memberID) {
-		this.memberID = memberID;
-	}
-	public int getQuantityPurchased() {
-		return quantityPurchased;
-	}
-	public void setQuantityPurchased(int quantityPurchased) {
-		this.quantityPurchased = quantityPurchased;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	
-	
-	
 
 }
