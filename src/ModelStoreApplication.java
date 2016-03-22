dfbaab050973749c6d475f742ee153074cb6899c
puja3001 <agarwal.puja3001@gmail.com>
2016-03-22 15:06:57 +0800

    Removed changes

--- src/ModelStoreApplication.java
+++ src/ModelStoreApplication.java
@@ -6,8 +6,6 @@ public class ModelStoreApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MainWindow mf = new MainWindow();
-		mf.setVisible(true);
 		//LoginWindow w = new LoginWindow(); 
 	    //w.setVisible(true);
 	    System.out.println("Test Print");
